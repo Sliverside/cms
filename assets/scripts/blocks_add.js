9d6df6d596459a8838c0ea8147da7df8a3387794
Lilian <moulan_l16@myges.fr>
2019-03-27 21:27:45 +0100

    improuve forms style

--- assets/scripts/blocks_add.js
+++ assets/scripts/blocks_add.js
@@ -1,6 +1,6 @@
-import {sfCollection} from './helpers/symfony-collection.js';
+// import {sfCollection} from './helpers/symfony-collection.js';
 import {formTheme} from './helpers/form-theme.js';
-sfCollection.init(); // {'collectionsSelector': 'form ul[data-prototype]'}
+// sfCollection.init(); // {'collectionsSelector': 'form ul[data-prototype]'}
 // import {DirectChildren} from './helpers/functions.js';
 
 // var addFieldButton = document.createElement('button');
